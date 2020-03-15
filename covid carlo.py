946cecf5f10fe77c918a3e489956fa0023a75920
Sanjay Patel <39928604+founderprof@users.noreply.github.com>
2020-03-15 18:16:12 -0500

    Update covid carlo.py

--- covid carlo.py
+++ covid carlo.py
@@ -38,9 +38,9 @@ def ListNonQuarantined(team):
 # Python program to calculate rates of infection
 
 
-iterations = 500      ## Number of iterations in the simulation
+iterations = 100      ## Number of iterations in the simulation
 days = 100            ## Number of days to simulate
-teamSize = 10         ## Number of people on the whole team
+teamSize = 20         ## Number of people on the whole team
 
 p0 = 0.005            ## Probability of infection from the population at large
 p1 = 0.50             ## Probability of infection from the office, from an infected co-worker
@@ -76,97 +76,100 @@ thetaAsym = (incubationSigma**2) / incubationTime
 alphaAsym = incubationTime / thetaAsym
 betaAsym = 1/thetaAsym 
 
-print("Running Covid Carlo.... with", iterations, "iterations on a team of", teamSize, "working for", days, "days.")
 
-for m in range(1, 5):
-   mode = m;
+print("Days, Mode, Period, TeamSize, Work, Healthy")
+for mode in range(1, 5):
+   for alternatingPeriod in range (1, 10):
+      for teamSize in range (10, 110, 10):
 
-   # Aggregate Statistics
-   numHealthy = 0;
-   personDays = 0
+         # Aggregate Statistics
+         numHealthy = 0;
+         personDays = 0
 
-   for i in range(0, iterations):
+         for i in range(0, iterations):
 
-      # Initialize everything
+            # Initialize everything
 
-      team = [0] * teamSize
-      dayInfected = [0] * teamSize      # day of infection
-      atOffice = [False] * teamSize     # who is the office on a given day (teams)
-      infectedInterval = [0] * teamSize # For an individual, how many days between infection and being infectious
-      asymptomInterval = [0] * teamSize # For an individual, how many days before symptoms appear
+            team = [0] * teamSize
+            dayInfected = [0] * teamSize      # day of infection
+            atOffice = [False] * teamSize     # who is the office on a given day (teams)
+            infectedInterval = [0] * teamSize # For an individual, how many days between infection and being infectious
+            asymptomInterval = [0] * teamSize # For an individual, how many days before symptoms appear
 
-      # Initialize who is at work
-      if (mode == 1):
-         atOffice = [True] * teamSize   # Everyone at the office 
-      elif (mode == 2 or mode == 3):
-         for j in range(0, int(len(team)/2)):
-            atOffice[j] = True          # teamA is at work on day 0
+            # Initialize who is at work
+            if (mode == 1):
+               atOffice = [True] * teamSize   # Everyone at the office 
+            elif (mode == 2 or mode == 3):
+               for j in range(0, int(len(team)/2)):
+                  atOffice[j] = True          # teamA is at work on day 0
 
-      ## Simulate each work day
-      for d in range(0, days):
+            ## Simulate each work day
+            for d in range(0, days):
 
-         # Determine which people are at work
-         # Time to switch teams!
-         if (d % alternatingPeriod):  
-            # Fixed teams, periodic days
-            if (mode == 2):
+               # Determine which people are at work
+               # Time to switch teams!
+               if (d % alternatingPeriod):  
+                  # Fixed teams, periodic days
+                  if (mode == 2):
+                     for j in range (0, len(team)):
+                        atOffice[j] = not atOffice[j]
+
+                  # Fixed teams, random days
+                  elif (mode == 3):
+                     # Swap teams based on a coin flip
+                     if (random.random() > 0.5):  
+                        for j in range (0, len(team)):
+                           atOffice[j] = not atOffice[j]
+
+                  # Random teams, random days
+                  elif (mode == 4):
+                     atOffice = [False] * teamSize
+                     nonQuarantined = ListNonQuarantined(team)
+                     teamList = random.sample(nonQuarantined, int(len(nonQuarantined)/2))
+                     for i in teamList:
+                        atOffice[i] = True
+
+               # Update intervals
+               for j in range(0, len(team)):
+                  if (infectedInterval[j] > 0):
+                     infectedInterval[j] -= 1 
+                  if (asymptomInterval[j] > 0):
+                     asymptomInterval[j] -= 1
+
+               # Transtions from Healthy to Infected
+               numInfected = CountInfectious(team)
                for j in range (0, len(team)):
-                  atOffice[j] = not atOffice[j]
-
-            # Fixed teams, random days
-            elif (mode == 3):
-               # Swap teams based on a coin flip
-               if (random.random() > 0.5):  
-                  for j in range (0, len(team)):
-                     atOffice[j] = not atOffice[j]
-
-            # Random teams, random days
-            elif (mode == 4):
-               atOffice = [False] * teamSize
-               nonQuarantined = ListNonQuarantined(team)
-               teamList = random.sample(nonQuarantined, int(len(nonQuarantined)/2))
-               for i in teamList:
-                  atOffice[i] = True
-
-         # Update intervals
-         for j in range(0, len(team)):
-            if (infectedInterval[j] > 0):
-               infectedInterval[j] -= 1 
-            if (asymptomInterval[j] > 0):
-               asymptomInterval[j] -= 1
-
-         # Transtions from Healthy to Infected
-         numInfected = CountInfectious(team)
-         for j in range (0, len(team)):
-               if (team[j] == Healthy):
-                  if (random.random() < p0):  ## New external infection
-                     team[j] = Infected
-                     infectedInterval[j] = int(round(numpy.random.gamma(alphaSerial, 1/betaSerial)))
-                     asymptomInterval[j] = int(round(numpy.random.gamma(alphaAsym, 1/betaAsym)))
-                  elif (atOffice[j]):         ## Infection at work!
-                     for k in range (0, numInfected):
-                        if (random.random() < p1):
+                     if (team[j] == Healthy):
+                        if (random.random() < p0):  ## New external infection
                            team[j] = Infected
                            infectedInterval[j] = int(round(numpy.random.gamma(alphaSerial, 1/betaSerial)))
                            asymptomInterval[j] = int(round(numpy.random.gamma(alphaAsym, 1/betaAsym)))
-         # Transition from Infected to Infectious
-         for j in range(0, len(team)):
-            if (team[j] == Infected and infectedInterval[j] == 0):
-               team[j] = Infectious
+                        elif (atOffice[j]):         ## Infection at work!
+                           for k in range (0, numInfected):
+                              if (random.random() < p1):
+                                 team[j] = Infected
+                                 infectedInterval[j] = int(round(numpy.random.gamma(alphaSerial, 1/betaSerial)))
+                                 asymptomInterval[j] = int(round(numpy.random.gamma(alphaAsym, 1/betaAsym)))
+
+               # Transition from Infected to Infectious
+               for j in range(0, len(team)):
+                  if (team[j] == Infected and infectedInterval[j] == 0):
+                     team[j] = Infectious
+               
+               # Transition from Infected/Infectious to Quarantined
+               for j in range(0, len(team)):
+                  if ((team[j] == Infected or team[j] == Infectious) and
+                      (asymptomInterval[j]==0) and
+                      (random.random() > (1.0-pQ))):
+                      team[j] = Quarantined
+
+               for j in range(0, len(team)):
+                  if ((team[j] == Healthy or team[j] == Infected) and atOffice[j]):
+                     personDays += 1
+
+            for j in range(0, len(team)):      
+               if (team[j] == Healthy):
+                  numHealthy += 1
+                  
+         print(days,", ", mode,", ", alternatingPeriod, ", ", teamSize, ", ", (personDays/iterations)/(teamSize*days), ", ", (numHealthy/iterations)/teamSize)
          
-         # Transition from Infectious to Quarantined
-         for j in range(0, len(team)):
-            if ((team[j] == Infected or team[j] == Infectious) and
-                (asymptomInterval[j]==0) and
-                (random.random() > (1.0-pQ))):
-                team[j] = Quarantined
-
-         for j in range(0, len(team)):
-            if ((team[j] == Healthy or team[j] == Infected) and atOffice[j]):
-               personDays += 1
-
-      for j in range(0, len(team)):      
-         if (team[j] == Healthy):
-            numHealthy += 1
-            
-   print("Mode", mode, "PersonDays of Work", personDays/iterations, " Number Healthy", numHealthy/iterations)
